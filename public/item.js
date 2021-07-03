b391d1695579ec58ba2122ba168045d99c4a8739
Sean <sean@voltagency.co>
2021-07-02 20:52:10 -0500

    Got the basic layout of the site working, and defined a few classes.

---
+++ public/item.js
@@ -0,0 +1,16 @@
+
+class Item{
+    /**
+     * 
+     * @param {string} name
+     * @param {object} time
+     * @param {object} duration
+     */
+    constructor(name, time, duration){
+        this.name = name;
+        this.time = time;
+        this.duration = duration;
+
+    }
+    
+}
---
+++ public/time.js
@@ -0,0 +1,27 @@
+
+class Day{
+    /**
+     * @param {array} item_list
+     */
+    constructor(itemList){
+        this.itemList = itemList;
+    }
+
+    /**
+     * @param {object} item 
+     */
+    insertItem(item){
+
+    }
+
+}
+
+class Week{
+    /**
+     * @param {array} days
+     */
+    constructor(days){
+        this.days = days
+    }
+}
+
