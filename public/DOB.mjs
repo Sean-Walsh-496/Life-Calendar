4780b98922da33102c34d2bf2a78e553d28cab1c
Sean <sean@voltagency.co>
2021-07-05 18:09:54 -0500

    Debugged DOB and added logic to extract user's DOB in index.

--- public/DOB.mjs
+++ public/DOB.mjs
@@ -1,6 +1,7 @@
 import popupWindow from "./popup.mjs";
 
 
+
 export class DateOfBirthWindow extends popupWindow {
     constructor($el){
         super($el);
@@ -22,24 +23,27 @@ export class DateOfBirthWindow extends popupWindow {
      * @returns {boolean} 
      */
     isValidDate($el, type){
+        type = type.toLowerCase();
+        let value = parseInt($el.value);
 
-        type = type.toLocaleLowerCase();
-        value = parseInt($el.value);
+        if (!isNaN(value)){
+            if (type == "day" || type == 'd'){
+                return value > 0 && value < 32; // sus
+            }
+            else if (type == "month" || type == 'm'){
+                return value > 0 && value < 13;
+            }
+            else if (type == "year" || type == 'y'){
+                let curYear = parseInt(new Date().getFullYear());
+                return value > curYear - 80 && value < curYear;
+            }
+            else {
+                console.error(`Invalid date type (${type}) passed in!`)
+                return false;
+            }  
+        }
 
-        if (type == "day" || type == 'd'){
-            return value > 0 && value < 32; // sus
-        }
-        else if (type == "month" || type == 'm'){
-            return value > 0 && value < 13;
-        }
-        else if (type == "year" || type == 'y'){
-            let curYear = parseInt(new Date().getFullYear());
-            return value > curYear - 80 && value < curYear;
-        }
-        else {
-            console.error(`Invalid date type (${type}) passed in!`)
-            return false;
-        }
+        return false;
     }
 
     /**
@@ -47,12 +51,14 @@ export class DateOfBirthWindow extends popupWindow {
      * @returns {boolean}
      */
     isAllFilled(){
-        let dateList = [this.$day, this.$month, this.$year];
-        
+        let dateList = [[this.$day, 'd'], [this.$month, 'm'], [this.$year, 'y']];
+        let returnValue = true;
+
+
         dateList.forEach(el => {
-            if (! this.isValidDate(el)) return false;
+            if (! this.isValidDate(...el)) returnValue = false;
         });
-        return true;
+        return returnValue;
     }
 
     /**
--- public/index.mjs
+++ public/index.mjs
@@ -1,7 +1,17 @@
 import PopupWindow from "./popup.mjs";"./popup.mjs";
 import {DateOfBirthWindow} from "./DOB.mjs";
 
+//variable declarations =================================================
 let $popUp = new DateOfBirthWindow(document.getElementById("DOB-popup"));
-console.log($popUp);
+let DOB;
+$popUp.$el.addEventListener("input", e =>{
+    if ($popUp.isAllFilled()){
+        DOB = $popUp.getDate();
+        $popUp.changeVisibility();
+    }
+
+});
+
+
 $popUp.changeVisibility();
 $popUp.center();
