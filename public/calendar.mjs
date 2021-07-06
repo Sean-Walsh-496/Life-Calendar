9e160ff23ef46f4177bae0a77c020688b246b6e5
Sean <sean@voltagency.co>
2021-07-06 16:36:45 -0500

    Added dividers at every 20 years with labels.

--- public/calendar.mjs
+++ public/calendar.mjs
@@ -1,7 +1,7 @@
 const weeksInYear = 52;
-const weekTailwind = "h-full w-2 border border-black ml-0.5 mr-0.5"
-const weekListTailwind = "m-0.5 flex max-w-6xl w-9/12 h-3 justify-between items-center"
-
+const weekTailwind = "h-full w-2 border border-black ml-0.5 mr-0.5";
+const weekListTailwind = "m-0.5 flex max-w-6xl w-full h-3 justify-between items-center";
+const dividerTailwind = "max-w-6xl w-full h-1";
 
 export class Calendar{
     constructor(weeks = 80 * weeksInYear){
@@ -28,6 +28,12 @@ export class Calendar{
             let $year = this.createYear();
             for (let j = 0; j < 52; j++){
                 $year.appendChild(this.createWeek());
+            }
+            if (i % 20 == 0){
+                let divider = document.createElement("div");
+                divider.className = dividerTailwind;
+                divider.style.backgroundColor = "#4B5563";
+                this.$el.append(divider);
             }
             this.$el.append($year);
         }
