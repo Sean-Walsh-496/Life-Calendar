f22e26ae6911b1f4fd79f9abdf046c8244c4b6a7
Sean <sean@voltagency.co>
2021-07-06 19:19:42 -0500

    Created getSizes function, made main run only after DOM is rendered, and allow the highlighting of clicked cells

--- public/index.mjs
+++ public/index.mjs
@@ -1,12 +1,38 @@
 import PopupWindow from "./popup.mjs";"./popup.mjs";
 import {DateOfBirthWindow} from "./DOB.mjs";
-import {Calendar} from "./calendar.mjs";
+import {Calendar, dividerTailwind, weekTailwind, weeksInYear} from "./calendar.mjs";
 
 //variable declarations =================================================
 let $popUp = new DateOfBirthWindow(document.getElementById("DOB-popup"));
 let DOB, numWeeks;
-const calendar = new Calendar();
+const $calendar = new Calendar();
 
+//Functions ============================================================
+function getSizes(){
+    const bounds = $calendar.$el.getBoundingClientRect();
+    const dividerBounds = document.getElementsByClassName(dividerTailwind)[0].getBoundingClientRect();
+    const weekBounds = document.getElementsByClassName(weekTailwind)[0].getBoundingClientRect();
+
+    return {
+        calendar : {
+            pos : [bounds.x, bounds.y],
+            width : bounds.right - bounds.x,
+            height : bounds.bottom - bounds.y,
+        },
+        
+        divider : {
+            width : dividerBounds.right - dividerBounds.x,
+            height : dividerBounds.bottom - dividerBounds.y
+        },
+
+        week : {
+            width : weekBounds.right - weekBounds.x,
+            height : weekBounds.bottom - weekBounds.y
+        }
+
+
+    }
+}
 
 //Event listener definitions ===========================================
 $popUp.$el.addEventListener("input", e =>{
@@ -16,14 +42,31 @@ $popUp.$el.addEventListener("input", e =>{
         DOB = $popUp.getDate();
         numWeeks = Math.floor((new Date() - DOB) / (1000 * 60 * 60 * 24 * 7));
 
-        calendar.highlightUsed(numWeeks);
+        $calendar.highlightUsed(numWeeks);
     }
+
+});
+
+addEventListener("click", e => {
+    const marginY = 4, marginX = 3;
+    const sizes = getSizes();
+    const coords = [e.clientX - sizes.calendar.pos[0], e.clientY - sizes.calendar.pos[1]];
+
+    let col = Math.round((coords[0] / (sizes.calendar.width)) * weeksInYear);
+    let row = Math.round(coords[1] / (sizes.week.height + marginY)) - 1;
+
+    console.log($calendar);
+
+    $calendar.highlight($calendar.matrix[row][col]);
 
 });
 
 
 //main
-$popUp.changeVisibility();
-$popUp.center();
-calendar.populate();
+document.addEventListener("DOMContentLoaded", function(e){
+    $popUp.changeVisibility();
+    $popUp.center();
+    $calendar.populate();    
+});
+
 
