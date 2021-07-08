c506277e749d739fa9762d247641a1175a572b8f
Sean <sean@voltagency.co>
2021-07-07 19:15:40 -0500

    Created the basic outline for the weekview page and its corresponding js file

---
+++ public/weekview.mjs
@@ -0,0 +1,12 @@
+import {Day, Week} from "./time.mjs";
+
+//variable declarations =================================================
+const $week = new Week();
+console.log($week);
+//main ==================================================================
+
+function main(){
+
+}
+
+main();
