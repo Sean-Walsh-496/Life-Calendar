0a4dbc4452e4b0c790db9ca50c0f424309363b07
Sean <sean@voltagency.co>
2021-07-18 18:25:44 -0500

    can now change item colors

--- public/item-popup.mjs
+++ public/item-popup.mjs
@@ -2,13 +2,34 @@ import PopupWindow from "./popup.mjs";
 
 export class ItemPopup extends PopupWindow {
 
-    constructor($el, $item){
+    constructor($el){
         super($el);
-        this.$item = $item;
+        this.$cur = null;
+        this.$colorPalette = document.getElementById("color-palette");
+
         this.standardDims = {height: 150, width: 150};
         this.itemOffset = [15, 10];
+
+        this.initEventListeners();
         
     }
+
+    initEventListeners(){
+        this.$el.addEventListener("click", e => {
+            if (e.target.tagName == "BUTTON" && this.$colorPalette.contains(e.target)){
+                this.$cur.style.backgroundColor = window.getComputedStyle(e.target).backgroundColor;
+
+            }
+        });
+
+        document.addEventListener("click", e => {
+            if (! this.$el.contains(e.target) && e.target !== this.$cur){
+                this.deactivate();
+            }
+        });
+
+    }
+
 
     /**
      * 
@@ -18,14 +39,12 @@ export class ItemPopup extends PopupWindow {
     expand(newW=200, newH=200){
         this.$el.style.height = `${newH}px`;
         this.$el.style.width = `${newW}px`;
-
-
     }
 
     shrink(){
         let {$el, standardDims} = this;
-        $el.style.height = `${standardDims.height}px`;
-        $el.style.width = `${standardDims.width}px`;
+        $el.style.height = `${0}px`;
+        $el.style.width = `${0}px`;
 
     }
 
@@ -33,7 +52,7 @@ export class ItemPopup extends PopupWindow {
      * @returns {array}
      */
     getItemPos(){
-        let bounds = this.$item.getBoundingClientRect();
+        let bounds = this.$cur.getBoundingClientRect();
         let {itemOffset} = this;
 
         return [bounds.right + itemOffset[0], bounds.top + itemOffset[1]];
@@ -44,14 +63,20 @@ export class ItemPopup extends PopupWindow {
      * custom context menu.
      * @param {MouseEvent} e
      */
-    activate(){
+    activate($item){
+        this.$el.style.visibility = "visible";
+        this.$cur = $item;
         this.position(...this.getItemPos());
         this.$el.style.width = `${this.standardDims.width}px`;
         this.$el.style.height = `${this.standardDims.height}px`;
+        this.$el.style.color = "black";
     }
 
-    deactive(){
-        
+    deactivate(){
+        this.$cur = null;
+        this.shrink();
+        this.$el.style.color = "transparent";
+
     }
 
 }
