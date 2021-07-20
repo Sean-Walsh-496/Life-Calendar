b5f6b4048b6d326851294011e1bc73dfb210e451
Sean <sean@voltagency.co>
2021-07-20 13:33:37 -0500

    added a save button

--- public/weekview.mjs
+++ public/weekview.mjs
@@ -1,10 +1,12 @@
 import {Day, Week} from "./time.mjs";
 import { ItemPopup } from "./item-popup.mjs";
+import { SaveButton } from "./save.mjs";
 
 //variable declarations =================================================
 const $week = new Week();
 const $container = document.getElementById("day-container");
 export const $itemPop = new ItemPopup(document.getElementById("edit-popup"));
+const $saveButton = new SaveButton($week);
 
 // event listeners
 
