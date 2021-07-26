a8bc4609f9c4e6411bf95105d665bc0b1a983afe
Sean <sean@voltagency.co>
2021-07-26 10:11:55 -0500

    made the DOB part of the user object

--- public/classes/user.mjs
+++ public/classes/user.mjs
@@ -1,16 +1,25 @@
 export class User{
-    constructor(name, password, years=80){
+
+    /**
+     * 
+     * @param {string}name 
+     * @param {string} password 
+     * @param {string} DOB 
+     */
+    constructor(name, password, DOB){
         this.name = name;
         this.password = password;
         this.years = {};
         this.templates = {};
+        this.DOB = DOB;
     }
     getSendable(){
         return {
             name: this.name,
             password: this.password,
             years: this.years,
-            templates: this.templates
+            templates: this.templates,
+            DOB: this.DOB
         };
     }
 }
