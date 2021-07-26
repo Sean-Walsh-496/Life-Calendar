57e69e76bfc26a2c12f5510b085d1783510df736
Sean <sean@voltagency.co>
2021-07-26 10:12:24 -0500

    checks for valid user input now, stopping users from inputting impossible birth dates or blank usernames

--- public/classes/register-window.mjs
+++ public/classes/register-window.mjs
@@ -12,6 +12,39 @@ export class RegisterWindow extends LandingPageBox{
 
     }
 
+    isValidDate($el, type){
+        type = type.toLowerCase();
+        let value = parseInt($el.value);
+
+        if (!isNaN(value)){
+            if (type == "day" || type == 'd'){
+                return value > 0 && value < 32; // sus
+            }
+            else if (type == "month" || type == 'm'){
+                return value > 0 && value < 13;
+            }
+            else if (type == "year" || type == 'y'){
+                let curYear = parseInt(new Date().getFullYear());
+                return value > curYear - 80 && value < curYear;
+            }
+            else {
+                console.error(`Invalid date type (${type}) passed in!`)
+                return false;
+            }  
+        }
+
+        return false;
+    }
+
+    isValidInput(){
+        let alphanumInputs = [this.$username, this.$password];
+        let numericInputs = [[this.$month, 'm'], [this.$day, 'd'], [this.$year, 'y']];
+
+        return (alphanumInputs.every(el => el.value != "")
+                                && 
+                numericInputs.every(el => this.isValidDate(...el)));
+    }
+
     initExtraEventHandlers(){
         this.$goBackButton.addEventListener("click", () =>{
             let $view = document.getElementById("sliding-view");
@@ -21,7 +54,16 @@ export class RegisterWindow extends LandingPageBox{
 
 
         this.$mainButton.addEventListener("click", async () => {
-            const user = new User(this.$username.value, this.$password.value);
+
+            if (! this.isValidInput()){
+                functions.transmitToWarning("invalid user input");
+                return;
+            }
+
+            let {$year, $month, $day} = this;
+            const date = new Date($year.value, $month.value - 1, $day.value);
+            const user = new User(this.$username.value, this.$password.value, date.toISOString());
+
 
             let response = await fetch("/register", {
                 method: "POST",
