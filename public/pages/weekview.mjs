8b3979aab1969d77d075886b6f5d01feca054dd0
Sean <sean@voltagency.co>
2021-07-29 09:41:31 -0500

    removed unnecessary code

--- public/pages/weekview.mjs
+++ public/pages/weekview.mjs
@@ -3,8 +3,6 @@ import { SaveButton } from "../classes/save.mjs";
 
 //variable declarations =================================================
 const $week = new Week();
-const $container = document.getElementById("day-container");
-console.log($week);
 const $saveButton = new SaveButton($week);
 
 
